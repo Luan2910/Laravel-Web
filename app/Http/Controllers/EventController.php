e579ad3632a4031e1ffd174ca2a24b4ad9d42535
Luan <luanvictor2910@hotmail.com>
2024-05-02 21:13:39 -0300

    Event features complete

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -27,6 +27,7 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items;
 
         // Image Upload
         if($request->hasFile('image') && $request->file('image')->isValid() ){
@@ -43,5 +44,13 @@ class EventController extends Controller
 
         return  redirect('/')->with('msg', 'Evento criado com sucesso!');
 
+    }
+
+    public function show($id) {
+
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
+        
     }
 }
