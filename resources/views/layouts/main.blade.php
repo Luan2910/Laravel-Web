486a4d543d76a24131091e1c99061c1c9c26aa2b
Luan <luanvictor2910@hotmail.com>
2024-05-16 20:28:20 -0300

    Simple Event Dashboard

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -72,7 +72,7 @@
         </main>
         
         <footer>
-            <p>Schedule.ev &copy; 2023</p>
+            <p>Schedule.ev &copy; 2024</p>
         </footer>
         <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
