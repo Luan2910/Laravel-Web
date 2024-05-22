20093eafed7b008c0ec60aa2f6712e135704c0a0
Luan <luanvictor2910@hotmail.com>
2024-05-21 22:20:05 -0300

    Edit event functions

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -57,7 +57,7 @@ class EventController extends Controller
 
         $event->save();
 
-        return  redirect('/')->with('msg', 'Evento criado com sucesso!');
+        return redirect('/')->with('msg', 'Evento criado com sucesso!');
 
     }
 
@@ -83,6 +83,35 @@ class EventController extends Controller
         
         Event::findOrFail($id)->delete();
 
-        return redirect('/dashboard')->with('msg', 'Evento Excluído com sucesso!');
+        return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!');
+    }
+
+    public function edit($id) {
+        
+        $event = Event::findOrFail($id);
+
+        return view('events.edit', ['event' => $event]);
+    }
+
+    public function update(Request $request) {
+
+        $data = $request->all();
+
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid() ){
+    
+            $requestImage = $request->image;
+            $extension = $requestImage->extension();
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+            $requestImage->move(public_path('img/events'), $imageName);
+            $data['image'] = $imageName;
+
+        }else{
+            $request->image = "events_placeholder.jpg";
+        }
+
+        Event::findOrFail($request->id)->update($data);
+
+        return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
     }
 }
